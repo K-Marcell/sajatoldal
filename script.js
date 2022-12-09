152a7edbc9527ea8dde04d0036d9080a583b6c9c
KadarM <101024695+K-Marcell@users.noreply.github.com>
2022-12-09 10:32:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var dataloaded = false;
-var datanumLoaded = 0;
 async function getCSData() {
     let obj;
     const result = await fetch('csharpdata.json');
@@ -29,24 +28,36 @@ async function getWEBData() {
     return obj.projects;
 }
 
+async function updateCard(cardNum, title, description, link)
+{
+    document.getElementById(`card${cardNum}`).children.item(0).textContent = title;
+    document.getElementById(`card${cardNum}`).children.item(1).textContent = description;
+    document.getElementById(`card${cardNum}`).children.item(2).textContent = link;
+}
+
 async function update(type) {
     if (type == 'CS') {
         dataloaded = false;
-        datanumLoaded = 0;
         getCSData().then(async (result) => {
-            for (var k = 0; k < result.length; k++) {
-                document.getElementById(`card${k + 1}`).children.item(0).textContent = result[k].title;
-                document.getElementById(`card${k + 1}`).children.item(1).textContent = result[k].description;
-                document.getElementById(`card${k + 1}`).children.item(2).href = result[k].link;
-            }
+            await updateCard(0,result[datanumLoaded+1].title, result[datanumLoaded+1].description, result[datanumLoaded+1].link);
+            await updateCard(0,result[datanumLoaded+2].title, result[datanumLoaded+2].description, result[datanumLoaded+2].link);
+            await updateCard(0,result[datanumLoaded+3].title, result[datanumLoaded+3].description, result[datanumLoaded+3].link);
+            await updateCard(0,result[datanumLoaded+4].title, result[datanumLoaded+4].description, result[datanumLoaded+4].link);
+            await updateCard(0,result[datanumLoaded+5].title, result[datanumLoaded+5].description, result[datanumLoaded+5].link);
             console.log(result);
             dataloaded = true;
             datanumLoaded = result.length;
+            if (datanumLoaded > 5){
+                datanumLoaded = 5;
+                document.getElementById(`nextP`).style.display = "block";
+            }
+            else {
+                document.getElementById(`nextP`).style.display = "none";
+            }
         });
     }
     if (type == 'PY') {
         dataloaded = false;
-        datanumLoaded = 0;
         getPYData().then(async (result) => {
             for (var k = 0; k < result.length; k++) {
                 document.getElementById(`card${k + 1}`).children.item(0).textContent = result[k].title;
