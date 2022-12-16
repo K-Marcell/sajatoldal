af03106844279aa643ad83c9358ed47f94d13c85
Marcii0 <101024695+K-Marcell@users.noreply.github.com>
2022-12-16 01:30:35 +0100

    Made it a lot better

--- script.js
+++ script.js
@@ -1,107 +1,54 @@
 var dataloaded = false;
 var datanumLoaded = 0;
-async function getCSData() {
+async function getData(type) {
     let obj;
-    const result = await fetch('csharpdata.json');
+    const result = await fetch('data/data.json');
     obj = await result.json();
-    console.log(obj);
-    return obj.projects;
+    switch (type) {
+        case 'CS':
+            return obj.CSprojects;
+        case 'PY':
+            return obj.PYprojects;
+        case 'SQL':
+            return obj.SQLprojects;
+        case 'HTML':
+            return obj.HTMLprojects;
+    }
 }
-async function getPYData() {
-    let obj;
-    const result = await fetch('pydata.json');
-    obj = await result.json();
-    console.log(obj);
-    return obj.projects;
-}
-async function getSQLData() {
-    let obj;
-    const result = await fetch('sqldata.json');
-    obj = await result.json();
-    console.log(obj);
-    return obj.projects;
-}
-async function getWEBData() {
-    let obj;
-    const result = await fetch('webdata.json');
-    obj = await result.json();
-    console.log(obj);
-    return obj.projects;
-}
-
-async function updateCard(cardNum, title, description, link) {
-    document.getElementById(`card${cardNum}`).children.item(0).textContent = title;
-    document.getElementById(`card${cardNum}`).children.item(1).textContent = description;
-    document.getElementById(`card${cardNum}`).children.item(2).textContent = link;
-}
-
+const removeChildren = (parent) => {
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
+    }
+};
 async function update(type) {
-    if (type == 'CS') {
-        dataloaded = false;
-        getCSData().then(async (result) => {
-            for (var k = 0; k < result.length; k++) {
-                document.getElementById(`card${k + 1}`).children.item(0).textContent = result[k].title;
-                document.getElementById(`card${k + 1}`).children.item(1).textContent = result[k].description;
-                document.getElementById(`card${k + 1}`).children.item(2).href = result[k].link;
-            }
-            console.log(result);
-            dataloaded = true;
-            datanumLoaded = result.length;
-        });
-    }
-    if (type == 'PY') {
-        dataloaded = false;
-        getPYData().then(async (result) => {
-            for (var k = 0; k < result.length; k++) {
-                document.getElementById(`card${k + 1}`).children.item(0).textContent = result[k].title;
-                document.getElementById(`card${k + 1}`).children.item(1).textContent = result[k].description;
-                document.getElementById(`card${k + 1}`).children.item(2).href = result[k].link;
-            }
-            console.log(result);
-            dataloaded = true;
-            datanumLoaded = result.length;
-        });
-    }
-    if (type == 'WEB') {
-        dataloaded = false;
-        datanumLoaded = 0;
-        getWEBData().then(async (result) => {
-            for (var k = 0; k < result.length; k++) {
-                document.getElementById(`card${k + 1}`).children.item(0).textContent = result[k].title;
-                document.getElementById(`card${k + 1}`).children.item(1).textContent = result[k].description;
-                document.getElementById(`card${k + 1}`).children.item(2).href = result[k].link;
-            }
-            console.log(result);
-            dataloaded = true;
-            datanumLoaded = result.length;
-        });
-    }
-    if (type == 'SQL') {
-        dataloaded = false;
-        datanumLoaded = 0;
-        getSQLData().then(async (result) => {
-            for (var k = 0; k < result.length; k++) {
-                document.getElementById(`card${k + 1}`).children.item(0).textContent = result[k].title;
-                document.getElementById(`card${k + 1}`).children.item(1).textContent = result[k].description;
-                document.getElementById(`card${k + 1}`).children.item(2).href = result[k].link;
-            }
-            console.log(result);
-            dataloaded = true;
-            datanumLoaded = result.length;
-        });
-    }
+    document.getElementById('chosenTask').style.display = 'none';
+    document.getElementById('tasksCont').style.display = 'block';
+    getData(type).then(async (result) => {
+        const selectComp = document.getElementById('selectTask');
+        removeChildren(selectComp);
+        document.getElementById('cardType').innerHTML = type;
+        console.log(result);
+        for (var k = 0; k < result.length; k++) {
+            let newOption = new Option(`${result[k]['title']}`, k);
+            selectComp.add(newOption, undefined);
+        }
+    });
 }
 
-function checkVisibility() {
-    if (dataloaded) {
-        for (var k = 1; k < datanumLoaded + 1; k++) {
-            document.getElementById(`card${k}`).style.display = "block";
-        }
-    }
-    else {
-        for (var k = 1; k < 5; k++) {
-            document.getElementById(`card${k}`).style.display = "none";
-        }
-    }
+async function displayTask(element) {
+    document.getElementById('chosenTask').style.display = 'block';
+    let index = element.selectedIndex;
+    getData(document.getElementById('cardType').innerHTML).then(async (result) => {
+        document.getElementById('taskTitle').innerHTML = result[index]['title'];
+        document.getElementById('taskDescription').innerHTML = result[index]['description'];
+        document.getElementById('taskButton').href = result[index]['link'];
+        document.getElementById('taskData').innerHTML = result[index]['data'];
+    });
 }
 
+async function openTaskSRC() {
+    let index = document.getElementById('selectTask').selectedIndex;
+    getData(document.getElementById('cardType').innerHTML).then(async (result) => {
+        window.open(result[index]['link'], '_blank');
+    });
+} 
